ccc43b2fbe19ede19b16394d524dc979ad173054
ShevDenny <sdenny904@gmail.com>
2021-09-23 21:20:24 -0400

    started

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+
+    return (
+        <div className="header-div">
+            <header className="header">
+                Shevon Denny
+                <nav className="nav-bar">
+
+                </nav>
+            </header>
+
+        </div>
+    )
+}
