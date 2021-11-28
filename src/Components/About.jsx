d8e463b03bc3c20854f591fb793d95c917aab8e4
ShevDenny <sdenny904@gmail.com>
2021-11-28 15:02:18 -0500

    done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,10 +9,10 @@ export default function About() {
                 <img className="home-pic" src={parkpic} alt="profile-pic" loading="lazy"
                 />
                 <div className="home-info">
-                    <p className="brief-about">Hello! My name is Shevon and I'm a full stack software engineer. My desire to learn and passion for problem solving and creating led me to pursue a career in software engineering. Now that I have made the decision to step into the tech world I plan to continue learning and building my skillset to eventually be in a position to help others like myself, maybe start my own business one day! 
+                    <p className="brief-about">Hello! My name is Shevon and I'm a full stack software engineer. My desire to learn and passion for problem solving led me to pursue a career in software engineering. Now that I have made the decision to step into the tech world I plan to continue learning and building my skillset. I aspire to be in a position to help others like myself, maybe start my own business one day! 
                     <br></br>
                     <br></br>
-                    When I'm not coding I'm working on my personal fashion brand, Vonshe, designing and constructing custom garments for women. It brings me joy bringing a vision to life and making women feel empowered and included in Fashion! I'm also a DIY enthusiast, I love to unwind by either reading a good book, or tending to my 50 plus houseplants! 
+                    When I'm not coding I'm working on my personal fashion brand <i>Vonshe</i>, designing and constructing custom garments for women. It brings me joy bringing a vision to life and making women feel empowered and included in Fashion! I'm also a DIY enthusiast, I love to unwind by either reading a good book or tending to my 50 plus houseplants! 
                     <br></br>
                     <br></br>
                     <i>Wanna work with me? Have any book recommendations? Or just want to chat? Feel free to email me shevoncdenny@gmail.com</i>
